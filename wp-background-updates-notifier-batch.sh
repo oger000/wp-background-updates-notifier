def02de013c87721c3a5bf726758e618db305796
oger000 <gerhard.oettl@ogersoft.at>
2017-09-17 21:14:49 +0200

    achtsamkeitscoaching

--- wp-background-updates-notifier-batch.sh
+++ wp-background-updates-notifier-batch.sh
@@ -5,3 +5,4 @@ cd "`dirname $0`"
 ./wp-background-updates-notifier-start.php gruene-burgschleinitz-kuehnring.at
 ./wp-background-updates-notifier-start.php christoph-koeck.at
 ./wp-background-updates-notifier-start.php schule-der-wertschaetzung.at
+./wp-background-updates-notifier-start.php achtsamkeitscoaching.at
